abf7f8b148cfc5f63dd4086cb952def4e6bd7da8
Borges <gustavoborges98@hotmail.com>
2020-07-02 22:38:36 -0300

    Desafios 03

---
+++ Works/aulajs/Desafios/desafios-03/buscaportecnologias.js
@@ -0,0 +1,17 @@
+const usuarios = [
+    { nome: "Carlos", tecnologias: ["HTML", "CSS"] },
+    { nome: "Jasmine", tecnologias: ["JavaScript", "CSS"] },
+    { nome: "Tuane", tecnologias: ["HTML", "Node.js"] }
+  ];
+
+  function checaSeUsuarioUsaCSS(usuario) {
+    return usuario['tecnologias'].includes('CSS')
+  }
+
+  for (let i = 0; i < usuarios.length; i++) {
+    const usuarioTrabalhaComCSS = checaSeUsuarioUsaCSS(usuarios[i]);
+  
+    if (usuarioTrabalhaComCSS) {
+      console.log(`O usuário ${usuarios[i].nome} trabalha com CSS`);
+    }
+  }

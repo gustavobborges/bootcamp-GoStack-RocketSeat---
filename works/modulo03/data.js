a0e7e20d9258aecb780b0f91893aeb85f3207554
Borges <gustavoborges98@hotmail.com>
2020-08-18 23:23:57 -0300

    Dados dinâmicos

---
+++ works/modulo03/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "57min",
+        price: "Free"
+    },
+    {
+        id: "vqrjFnq3-uo",
+        title: "Criando Player de Audio | Diego",
+        duration: "38min",
+        price: "Free"
+    },
+    {
+        id: "2alg7MQ6_sI",
+        title: "Git e Github na Prática | Mayk Brito",
+        duration: "57min",
+        price: "Free"
+    },
+    {
+        id: "GSqR2i-Pq6o",
+        title: "Desvendando a variável This | Mayk Brito",
+        duration: "57min",
+        price: "Free"
+    },
+    {
+        id: "GykTLqODQuU",
+        title: "Criando Templates | Mayk Brito",
+        duration: "27min",
+        price: "Free"
+    },
+    {
+        id: "OXxul6AvXNs",
+        title: "Typescript + React | Diego",
+        duration: "45min",
+        price: "Free"
+    }
+]
--- works/modulo03/server.js
+++ works/modulo03/server.js
@@ -2,21 +2,35 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false //para ler o html das páginas
 })
 
 server.get("/", function(req, res) {
-    return res.render("about")
+    const about = {
+        avatar_url: "https://avatars1.githubusercontent.com/u/52687806?s=460&u=6caf52605d04364cd60e72fc6e85d9435f9de73e&v=4",
+        name: "Gustavo Borges",
+        role: "Developer - iBridge Technology",
+        description: 'Programador full-stack com foco em UI e UX. Colaborador da <a href="www.ibridge.com.br" target="_blank">iBridge</a> e apaixonado por tecnologia!',
+        links: [
+            { name: "Github", url: "https://www.github.com/gustavobborges"},
+            { name: "Linkedin", url: "https://www.linkedin.com/in/gustavo-de-borja-borges-962726145/"}
+        ]
+    }
+
+    return res.render("about", { about: about })
 })
 
 server.get("/portfolio", function(req, res) {
-    return res.render("portfolio")
+
+    return res.render("portfolio", {items: videos})
 })
 
 server.use(function(req, res) {

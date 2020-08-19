083383557c1e191b60354cabbfa70d1450d053de
Borges <gustavoborges98@hotmail.com>
2020-08-19 00:00:24 -0300

    desafio 3-2

--- works/modulo03/desafios/desafio-3-2/server.js
+++ works/modulo03/desafios/desafio-3-2/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const courses = require("./data")
 
 server.use(express.static('public'))
 
@@ -12,18 +13,35 @@ nunjucks.configure("views", {
 })
 
 server.get("/", function(req, res) {
-    return res.render("about")
+    const about = {
+        name: "Rocketseat",
+        url_img: "https://yt3.ggpht.com/a/AATXAJwSX58F1bJzQBZYiioJa36NuvznvNpsEVDfmc7qfg=s900-c-k-c0xffffffff-no-rj-mo",
+        description: "Empresa de educação focada em desenvolimento full-stack com as melhores tecnologias",
+        techs: [
+            { name: "Javascript" },
+            { name: "Typescript" },
+            { name: "Node Js" },
+            { name: "React" },
+            { name: "React Native" },
+        ],
+        links: [
+            {name: "Github", url: "https://www.github.com/gustavobborges"},
+            {name: "Linkedin", url: "https://www.linkedin.com/in/gustavo-de-borja-borges-962726145/"}
+        ]
+    }
+
+    return res.render("about", { about: about })
 })
 
 server.get("/courses", function(req, res) {
-    return res.render("courses")
+    return res.render("courses", { items: courses })
 })
 
 server.use(function(req, res) {
     res.status(404).render("not-found")
 })
 
-server.listen(5050, function() {
+server.listen(5000, function() {
     console.log("server is running...")
 })
 

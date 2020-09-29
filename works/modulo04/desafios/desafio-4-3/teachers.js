2151577a3e1051d997fd055d83cb16bc5cb15a61
Borges <gustavoborges98@hotmail.com>
2020-09-28 23:33:17 -0300

    alteração dos campos

--- works/modulo04/desafios/desafio-4-3/teachers.js
+++ works/modulo04/desafios/desafio-4-3/teachers.js
@@ -11,15 +11,17 @@ exports.post = function(req, res) {
         }
     }
 
-    let { name, age, schooling, classType, theme } = req.body
+    let { avatar_url, name, birth, schooling, classType, theme } = req.body
 
+    birth = Date.parse(birth)
     const created_at = Date.now()
     const id = Number(data.teachers.length + 1)
 
     data.teachers.push({
         id,
+        avatar_url,
         name,
-        age,
+        birth,
         schooling,
         classType,
         theme,

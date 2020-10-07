163e09d1896cbb690dd07c2c713ab1c51f6a40ab
Borges <gustavoborges98@hotmail.com>
2020-10-06 21:57:51 -0300

    atualizacao de dependencias

--- works/modulo04/desafios/desafio-4-6/teachers.js
+++ works/modulo04/desafios/desafio-4-6/teachers.js
@@ -2,6 +2,11 @@ const fs = require('fs')
 const data = require('./data.json')
 const { age, date, graduation } = require('./utils')
 
+//index
+exports.index = function(req, res) {
+    return res.render("teachers/index", { instructors: data.instructors })
+}
+
 //create
 exports.post = function(req, res) {
     const keys = Object.keys(req.body)
@@ -93,7 +98,8 @@ exports.put = function(req, res) {
     const teacher = {
         ...foundTeacher,
         ...req.body,
-        birth: Date.parse(req.body.birth)
+        birth: Date.parse(req.body.birth),
+        id: Number(req.body.id)
     }
 
     data.teachers[index] = teacher
